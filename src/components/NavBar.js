939196436be2764f3a72cc03f003cbefa592855e
hymced <hymced@gmail.com>
2023-06-23 17:43:30 +0200

    typo in comments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import './NavBar.css'
 const NavBar = () => {
     return <>
         <nav className="NavBar">
-            {/* use end prop because links stay active when they are subsets of other links */}
+            {/* use end prop because otherwise links stay active when they are subsets of other links */}
             <NavLink end to="/">HomePage</NavLink>
             <NavLink end to="/apartments">ApartmentsList</NavLink>
             <NavLink to="/apartments/create">CreateApartment</NavLink>
